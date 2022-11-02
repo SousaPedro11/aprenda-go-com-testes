0e5f5d9241a9bc86faaec9ff91b00647d3c135b8
Pedro Sousa <36779714+SousaPedro11@users.noreply.github.com>
2022-11-02 15:50:42 -0300

    refactor: add helper method to verify correct message

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	obtido := Ola("Pedro")
-	esperado := "Olá, Pedro!"
 
-	if obtido != esperado {
-		t.Errorf("obtido '%s' esperado '%s'", obtido, esperado)
+	verificaMensagemCorreta := func(t *testing.T, obtido, esperado string) {
+		t.Helper()
+		if obtido != esperado {
+			t.Errorf("obtido '%s', esperado '%s'", obtido, esperado)
+		}
 	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		esperado := "Olá, Chris!"
+		obtido := Ola("Chris")
+
+		verificaMensagemCorreta(t, obtido, esperado)
+	})
+
+	t.Run("diz 'Olá, Mundo!' quando uma string vazia for passada", func(t *testing.T) {
+		esperado := "Olá, Mundo!"
+		obtido := Ola("")
+
+		verificaMensagemCorreta(t, obtido, esperado)
+	})
 }
