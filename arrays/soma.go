dc200da482bab14ec021f27eba60182aee80ff61
Pedro Sousa <36779714+SousaPedro11@users.noreply.github.com>
2022-11-05 12:08:03 -0300

    feat: add sum elements in arrays

--- arrays/soma.go
+++ arrays/soma.go
@@ -13,3 +13,15 @@ func SomaTudo(numerosParaSomar ...[]int) (somas []int) {
 	}
 	return
 }
+
+func SomaTodoOResto(numerosParaSomar ...[]int) (somas []int) {
+	for _, numeros := range numerosParaSomar {
+		if len(numeros) == 0 {
+			somas = append(somas, 0)
+		} else {
+			final := numeros[1:]
+			somas = append(somas, Soma(final))
+		}
+	}
+	return
+}
--- arrays/soma_test.go
+++ arrays/soma_test.go
@@ -41,3 +41,24 @@ func TestSomaTudo(t *testing.T) {
 		t.Errorf("resultado %v, esperado %v", resultado, esperado)
 	}
 }
+
+func TestSomaTodoOResto(t *testing.T) {
+	verificarSomas := func(t *testing.T, resultado, esperado []int) {
+		t.Helper()
+		if !reflect.DeepEqual(resultado, esperado) {
+			t.Errorf("resultado %v, esperado %v", resultado, esperado)
+		}
+	}
+
+	t.Run("faz a soma dos elementos", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{1, 2}, []int{0, 9})
+		esperado := []int{2, 9}
+		verificarSomas(t, resultado, esperado)
+	})
+
+	t.Run("soma de um slice vazio", func(t *testing.T) {
+		resultado := SomaTodoOResto([]int{}, []int{3, 4, 5})
+		esperado := []int{0, 9}
+		verificarSomas(t, resultado, esperado)
+	})
+}
